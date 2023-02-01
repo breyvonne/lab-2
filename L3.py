d96274f37a1c85b780795c2f543fe651fdabbb0e
Breanna <123026437+breyvonne@users.noreply.github.com>
2023-02-01 16:12:29 -0500

    Add files via upload

---
+++ L3.py
@@ -0,0 +1,7 @@
+#Breanna Eafon
+import math
+x=math.tan(math.pi/4)
+numerator = math.sqrt(x+1)
+y=math.tan(math.pi/3)
+denominator= math.sqrt(y+math.sin(y))
+print (numerator/denominator)
---
+++ L4 (1).py
@@ -0,0 +1,7 @@
+#Breanna Eafon
+import math
+r=4
+a=4*math.sqrt(2)
+circle= math.pi*r**2
+square=a*a
+print (circle - square)
